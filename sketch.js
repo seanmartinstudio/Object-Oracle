b9eb6ef616ec70c82bae452931bb3605ca2a2bd2
Sean Martin <sean@seanmartinstudio.com>
2023-06-06 14:13:01 -0700

    adds CSS to center canvas

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 let mobilenet
-let parrot
+let video
+let label = ''
 
 const modelReady = () => {
-  console.log("Model is ready!")
-  mobilenet.predict(parrot, gotResults)
+  console.log("Model is ready!!!")
+  mobilenet.predict(gotResults)
 }
 
 const gotResults = (error, results) => {
@@ -12,27 +13,27 @@ const gotResults = (error, results) => {
   } else {
     console.log(results)
   }
-  let label = results[0].label
-  let probability = results[0].confidence
-  fill(0)
-  textSize(64)
-  createP(label)
-  createP(probability)
+  label = results[0].label
+  mobilenet.predict(gotResults)
 }
-
-
-const imageReady = () => image(parrot, 0, 0, width, height)
 
 function setup() {
-  createCanvas(640, 480);
-
-  //Creates webcame view
-  parrot = createCapture(VIDEO)
-  parrot.hide()
+  createCanvas(740, 580)
   background(0)
-  mobilenet = ml5.imageClassifier('MobileNet', modelReady)
+  video = createCapture(VIDEO)
+  video.hide()
+  // console.log("Video, ", video)
+  // video.position(windowWidth * .3, windowHeight/5)
+  mobilenet = ml5.imageClassifier('MobileNet', video, modelReady)
 }
 
-// function draw() {
-//   background(220);
-// }
+function draw() {
+  // image(video)
+  // fill(0)
+  // textSize(32)
+  // text(label, 10, height - 100)
+}
+
+
+//Have video on center of screen but not in canvas
+//Have canvas size of screen and have the labels writing at all times
